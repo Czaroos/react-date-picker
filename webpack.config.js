d33a276227d215cdeb4ab4bad3d6d81823461da0
Czaroos <czaroosik@gmail.com>
2020-10-06 16:14:04 +0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.scss', '.js'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+    publicPath: '/',
+  },
+};
